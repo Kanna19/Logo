be5af5cf318c26f4c08a615a6de577a5ffd1fdd8
Krishna Chaitanya <chaitu7261998@gmail.com>
2018-04-12 22:52:42 +0530

    Make turtle go forward and turn right

--- main.hs
+++ main.hs
@@ -5,6 +5,7 @@ import Graphics.UI.Gtk hiding (rectangle)
 import Graphics.Rendering.Cairo
 import Data.List
 import Data.Typeable
+import Text.Read
 
 main :: IO ()
 main = do
@@ -24,6 +25,11 @@ main = do
     inputPart <- textViewNew
     --textViewSetBorderWindowSize inputPart  TextWindowTop 200
 
+    canvas <- drawingAreaNew
+    widgetSetSizeRequest canvas 800 300
+
+    canvas `on` draw $ centreTurtle canvas
+
     btn <- buttonNew
     set btn [ buttonLabel := "Enter"]
     btn `on` buttonActivated $ do
@@ -32,12 +38,11 @@ main = do
         start <- textBufferGetStartIter inputBuffer
         end <- textBufferGetEndIter inputBuffer
         tempText <- textBufferGetText inputBuffer start end False
+        canvas `on` draw $ updateCanvas canvas tempText
+        widgetQueueDraw canvas
         textBufferInsert displayBuffer iter (tempText ++ "\n")
 
         return ()
-
-    canvas <- drawingAreaNew
-    widgetSetSizeRequest canvas 800 300
 
     displayScroll <- scrolledWindowNew Nothing Nothing
     containerAdd displayScroll displayPart
@@ -68,3 +73,48 @@ stringToMaybeString :: String -> Maybe String
 stringToMaybeString x
  | True = Just x
  | otherwise = Nothing
+
+updateCanvas :: DrawingArea -> String -> Render ()
+updateCanvas canvas command = do
+    let distance = if (take 2 command) == "fd"
+                    then read (drop 3 command) :: Double
+                    else 0
+
+    let angle = if (take 2 command) == "rt"
+                    then read (drop 3 command) :: Double
+                    else 0
+
+    rotate (angle*pi/180)
+    (w,h) <- getCurrentPoint
+    --(w,h) <- (100, 100)
+
+    lineTo w (h-distance)
+    stroke
+    markEnd w (h-distance)
+    where markEnd x y = do
+            setSourceRGB 0 1 0
+            moveTo x y
+            lineTo x y
+            stroke
+            setSourceRGB 1 0 0
+            moveTo x y
+            strokePreserve
+
+centreTurtle :: DrawingArea -> Render ()
+centreTurtle canvas = do
+    width'  <- liftIO $ widgetGetAllocatedWidth  canvas
+    height' <- liftIO $ widgetGetAllocatedHeight canvas
+    let width  = realToFrac width'
+        height = realToFrac height'
+
+    setSourceRGB 1 0 0
+    setLineWidth 4
+    setLineCap LineCapRound
+    setLineJoin LineJoinRound
+
+    moveTo (width/2) (height/2)
+    lineTo (width/2) (height/2)
+    strokePreserve
+
+renderToIO :: Render () -> IO ()
+renderToIO world = return ()
