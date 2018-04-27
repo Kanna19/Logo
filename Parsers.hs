f1bd0ef9fa4dd00af9d5de512fe79732683e461e
Kanna19 <kannasasuke19@gmail.com>
2018-04-27 22:54:44 +0530

    Provided seperate functions for forward and right

--- Parsers.hs
+++ Parsers.hs
@@ -11,21 +11,42 @@ import Text.Read
 
 updateCanvas :: DrawingArea -> String -> Render ()
 updateCanvas canvas command = do
-    let distance = if (take 2 command) == "fd"
-                    then read (drop 3 command) :: Double
-                    else 0
 
-    let angle = if (take 2 command) == "rt"
-                    then read (drop 3 command) :: Double
-                    else 0
+    if (take 2 command) == "fd" then
+        moveForward argument
+
+    else if (take 2 command) == "rt" then
+        turnRight argument
+
+    else
+        return ()
+
+        where argument = read(drop 3 command) :: Double
+
+
+moveForward distance = do
+
+    (w, h) <- getCurrentPoint
+
+    lineTo w (h - distance)
+    stroke
+
+    markEnd w (h - distance)
+    where markEnd x y = do
+            setSourceRGB 0 1 0
+            moveTo x y
+            lineTo x y
+            stroke
+            setSourceRGB 1 0 0
+            moveTo x y
+            strokePreserve
+
+turnRight angle = do
 
     rotate (angle*pi/180)
     (w,h) <- getCurrentPoint
-    --(w,h) <- (100, 100)
 
-    lineTo w (h-distance)
-    stroke
-    markEnd w (h-distance)
+    markEnd w h
     where markEnd x y = do
             setSourceRGB 0 1 0
             moveTo x y
