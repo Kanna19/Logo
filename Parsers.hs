2d1715a4ef5f3c33e0cefc13c4a8537fd8f3b96a
Krishna Chaitanya <chaitu7261998@gmail.com>
2018-04-28 01:07:43 +0530

    Added repeat functionality

--- Parsers.hs
+++ Parsers.hs
@@ -8,9 +8,42 @@ import Graphics.Rendering.Cairo
 import Data.List
 import Data.Typeable
 import Text.Read
+import Data.Maybe
+
+stringToCommands :: String -> (String, String)
+stringToCommands string = if ' ' `elemIndex` (drop 3 string) /= Nothing then
+                          splitAt (fromJust(' ' `elemIndex` (drop 3 string)) + 4) string
+                          else (string, "")
+
+stringToCommandss :: String -> (String, String)
+stringToCommandss string = splitAt (fromJust(' ' `elemIndex` string) + 1) string
+
+ioToRender :: IO () -> Render ()
+ioToRender _ = return ()
+repeatCommand :: DrawingArea -> String -> Render ()
+repeatCommand canvas commands = do
+    let (command, restString) = stringToCommands commands 
+
+    updateCanvas canvas command
+
+    if restString == "" then
+        return ()
+
+    else repeatCommand canvas restString
+
+repeatCommands :: DrawingArea -> Int -> String -> Render ()
+repeatCommands canvas times commands = do
+    if times > 0 then do
+        repeatCommand canvas commands
+        repeatCommands canvas (times-1) commands
+    else return ()
+
+
 
 updateCanvas :: DrawingArea -> String -> Render ()
 updateCanvas canvas command = do
+
+    ioToRender (putStrLn "Bom!")
 
     if (take 2 command) == "fd" then
         moveForward argument
@@ -21,10 +54,14 @@ updateCanvas canvas command = do
     else if (take 2 command) == "lt" then
         turnLeft argument
 
+    else if (take 6 command) == "repeat" then
+        repeatCommands canvas ((read repArg) :: Int) (init (tail repCom))
+
     else
         return ()
 
         where argument = read(drop 3 command) :: Double
+              (repArg,repCom) = stringToCommandss (drop 7 command)
 
 
 moveForward :: Double -> Render ()
