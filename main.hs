6c99308401a58bb84a98a0bed5c3e289c464f98a
ABForSure <cs16btech11006@iith.ac.in>
2018-04-29 00:00:53 +0530

    Added comments to main.hs

--- main.hs
+++ main.hs
@@ -23,31 +23,52 @@ main = do
     set window [ windowTitle         := "Logo"
                , windowResizable     := False ]
 
+    -- Create text view to display history
     displayPart <- textViewNew
 
+    -- Make display part uneditable
     textViewSetEditable displayPart False
+
+    -- Get text buffer from display part
     displayBuffer <- textViewGetBuffer displayPart
 
+    -- Create text view to take input
     inputPart <- textViewNew
-    --textViewSetBorderWindowSize inputPart  TextWindowTop 200
+ 
+    -- Create new Canvas
+    canvas <- drawingAreaNews
 
-    canvas <- drawingAreaNew
+    -- Set the size of the canvas
     widgetSetSizeRequest canvas 800 300
 
+    -- Render functions to execute on drawing canvas
     canvas `on` draw $ centreTurtle canvas
     canvas `on` draw $ clearScreen
     canvas `on` draw $ drawTurtle
 
+    -- Make commands.txt empty
     writeFile "commands.txt" ""
+
+    -- Open file in read write mode
     handle <- openFile "commands.txt" ReadWriteMode
+    
+    -- Initial position of the handle
     pos <- hGetPosn handle
 
+    -- Create enter button
     enterButton <- buttonNew
+
+    -- Set the text of enter button
     set enterButton [ buttonLabel := "Enter"]
 
+    -- Actions to be done when enter button is clicked
     enterButton `on` buttonActivated $ do
+
+        -- Set current position to the start of the file
         curPos <- hGetPosn handle
         hSetPosn pos
+
+        -- Check if EOF is detected
         isEnd <- hIsEOF handle
 
         commands <- if isEnd then
@@ -60,35 +81,59 @@ main = do
 
         hSetPosn curPos
         
+        -- Get text buffer from input part
         inputBuffer <- textViewGetBuffer inputPart
+
+        -- Iterator to iterate through text buffer
         iter <- textBufferGetStartIter displayBuffer
+
+        -- Start and end iterators
         start <- textBufferGetStartIter inputBuffer
         end <- textBufferGetEndIter inputBuffer
+        
+        -- Commands to be executed
         tempText <- textBufferGetText inputBuffer start end False
         
+        -- Send the entered commands to updateCanvas
         canvas `on` draw $ updateCanvas canvas (("repeat 1 [" ++ tempText) ++ "]")
+        
+        -- Draw turtle
         canvas `on` draw $ drawTurtle
+
+        -- Request redrawing of canvas
         widgetQueueDraw canvas
         
+        -- Write the input commands in commands.txt
         hPutStr handle (tempText ++ " ")
         textBufferInsert displayBuffer iter (tempText ++ "\n")
 
         return ()
 
+    -- Create enter button
     saveButton <- buttonNew
+
+    -- Set the text of the save button
     set saveButton [ buttonLabel := "Save"]
 
+    -- Create new surface to convert to image
     srf <- createImageSurface FormatARGB32 800 600
     
+    -- Actions to be done when save is clicked
     saveButton `on` buttonActivated $ do
-    
+        
+        -- Set the position of the handle to the start of the file
         hSetPosn pos
+
+        -- Get the commands
         commands <- hGetContents handle
         renderWith srf (updateCanvas canvas ("repeat 1 [clear " ++ commands ++ "]"))
         surfaceWriteToPNG srf "Picture.png"
+        
+        -- Exit after saving
         liftIO mainQuit
         return ()
 
+    -- Functions arranging the layout of the program
     displayScroll <- scrolledWindowNew Nothing Nothing
     containerAdd displayScroll displayPart
     widgetSetSizeRequest displayScroll 800 100
